9d6e467e21a56ee7021f80ea59417ec158ebe798
Dat Chau <datchau23101994@gmail.com>
2018-10-10 13:15:42 +0700

    tour

---
+++ resources/views/pages/layouts/footer.blade.php
@@ -0,0 +1,52 @@
+<div class="footer-top-area">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-3 col-sm-6">
+                <div class="footer-about-us">
+                    <h2>e<span>Shop</span></h2>
+                    <p>Website bán hàng điện tử các loại điện thoại, laptop và phụ kiện chính hãng, uy tín, giá cả cạnh tranh</p>
+                    <div class="footer-social">
+                        <a href="#" target="_blank"><i class="fa fa-facebook"></i></a>
+                        <a href="#" target="_blank"><i class="fa fa-twitter"></i></a>
+                        <a href="#" target="_blank"><i class="fa fa-youtube"></i></a>
+                        <a href="#" target="_blank"><i class="fa fa-linkedin"></i></a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 col-sm-6">
+                <div class="footer-menu">
+                    <h2 class="footer-wid-title">Điều hướng</h2>
+                    <ul>
+                        <li><a href="#">Tài khoản của tôi</a></li>
+                        <li><a href="#">Lịch sử mua hàng</a></li>
+                        <li><a href="#">Chính sách bảo hành</a></li>
+                        <li><a href="#">Chính sách thanh toán</a></li>
+                        <li><a href="#">Liên hệ</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-md-3 col-sm-6">
+                <div class="footer-menu">
+                    <h2 class="footer-wid-title">Danh mục</h2>
+                    <ul>
+                        <li><a href="#">Điện thoại</a></li>
+                        <li><a href="#">Máy tính</a></li>
+                        <li><a href="#">Phụ kiện</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-md-3 col-sm-6">
+                <div class="footer-newsletter">
+                    <h2 class="footer-wid-title">Góp ý, khiếu nại</h2>
+                    <p>Nếu có bất kỳ ý kiến góp ý hay khiếu nại vấn đề gì, xin hãy gửi mail vê cho chúng tôi!</p>
+                    <div class="newsletter-form">
+                        <form action="#">
+                            <textarea placeholder="Ý kiến đóng góp" name='mail' class="textarea-mail"></textarea>
+                            <input type="submit" value="Gửi">
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div> <!-- End footer top area -->
